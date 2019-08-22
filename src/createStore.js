f3e594b9c7343064af3d470394793dc2daa8b156
diphillipophillip <diphillipophillip@gmail.com>
2019-08-21 21:17:42 -0400

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,7 +1,27 @@
 export default function createStore(reducer) {
-  // add your code here
+    let state 
+    if (state === []) {
+      state = []
+    } else {
+      state = 0
+    }
+
+    function dispatch(action) {
+      state = reducer(state, action) 
+      render()
+    }
+
+    function getState() {
+      return state
+    }
+
+    return {
+      dispatch,
+      getState
+    }
 }
 
 function render() {
   const container = document.getElementById('container');
+  
 }
